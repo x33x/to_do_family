68ab3be3f75a8eb30ca21474b31793c16d4bd1d1
x33x <pavel.zz86@gmail.com>
2023-08-26 11:24:23 +0400

    Delete to_do_family folder

--- to_do_family/mytodo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def mytodo(request):
-    user = request.user
-    return render(request, 'mytodo/mytodo.html', {'user': user})
